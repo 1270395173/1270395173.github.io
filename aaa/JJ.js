c41dedadba2a3af9bbde7c815a12a8403d2be446
1270395173 <142875723+1270395173@users.noreply.github.com>
2023-08-22 15:37:12 +0800

    Add files via upload

---
+++ aaa/JJ.js
@@ -0,0 +1,13 @@
+function pow(){
+    for (let c=1;c==1;){
+        let a=prompt("输入a")
+        let b=prompt("输入b")
+        if (a==null || a== '' || b==null || b== ''){
+            alert('请输入正确的值')
+        }else{
+            alert(a ** b)
+        }
+    }
+
+}
+pow();
